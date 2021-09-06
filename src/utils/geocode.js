b63b7d3246829d88a4ef91d10395d106cb858368
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-09-07 01:07:04 +0700

    fixed bug Location not found
    - url

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,7 +5,7 @@ const request = require('postman-request');
 
 const geocode = (address, callback) => {
 
-    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidGhlZXJhcG9ubmFub2kxODUiLCJhIjoiY2tzdmp3cHIwMDZ0MjJ1cnd6OTR4YWh1aiJ9.cvM36CjtVK_X1sM800wInA&limit=1'
+    const url = `http://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoidGhlZXJhcG9ubmFub2kxODUiLCJhIjoiY2tzdmp3cHIwMDZ0MjJ1cnd6OTR4YWh1aiJ9.cvM36CjtVK_X1sM800wInA&limit=1`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -16,8 +16,8 @@ const geocode = (address, callback) => {
             callback(errorMessage, undefined)
         } else {
             callback(undefined, {
-                latitude: body.features[0].center[0],
-                longitude: body.features[0].center[1],
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
                 location: body.features[0].place_name
             })
         }
